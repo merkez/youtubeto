f081834b5f6313d212b30203aa48e16eb6ea550f
Ahmet Turkmen <f.ahmet.turkmen@icloud.com>
2020-11-23 17:08:23 +0100

    add description to CompressWithPIGZ function
    Signed-off-by: Ahmet Turkmen <f.ahmet.turkmen@icloud.com>

--- client/tar.go
+++ client/tar.go
@@ -4,11 +4,12 @@ type Tar struct {
 	c *Client
 }
 
-// exec executes an ExecFunc using 'youtube-dl'.
+// exec executes an ExecFunc using 'tar' command.
 func (tr *Tar) exec(args ...string) ([]byte, error) {
 	return tr.c.exec("tar", args...)
 }
 
+// CompressWithPIGZ using tar with pigz compress program to compress given data
 func (tr *Tar) CompressWithPIGZ(fileName, folderToCompress string) error {
 	cmds := []string{"--use-compress-program=pigz", "-cf", fileName, folderToCompress}
 	_, err := tr.exec(cmds...)
